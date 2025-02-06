f5ebaf28669ecb314ba9630e3823ca99ff2bac78
Jiku Godwill Nsanwi <jikugodwill@yahoo.com>
2025-02-06 17:35:14 +0100

    fmt

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfitg } */
 const nextConfig = {
   transpilePackages: ["react-tweet"],
-  swcMinify: true,
-  reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
 };
 
 export default nextConfig;
